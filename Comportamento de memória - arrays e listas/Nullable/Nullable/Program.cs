1feacc6c85a539add48c63ee4f331484954ed635
João Paulo Pacheco <joaopaulo.pacheco@outlook.com.br>
2020-10-12 17:10:23 -0300

    Adicionado sessao Comportamento de memoria

---
+++ Comportamento de memória - arrays e listas/Nullable/Nullable/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10.0;
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("X is null");
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("Y is null");
+        }
+    }
+}
