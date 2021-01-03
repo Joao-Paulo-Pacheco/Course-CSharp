273648be1834ff320db62abac960dc810d052041
João Paulo Pacheco <48422839+Joao-Paulo-Pacheco@users.noreply.github.com>
2021-01-02 21:43:07 -0300

    Add files via upload

---
+++ InputArquive/InputArquive/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace InputArquive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+                }
+
+                foreach (Product product in list)
+                {
+                    Console.WriteLine(product);
+                }
+            }
+        }
+    }
+}
